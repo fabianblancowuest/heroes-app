3f195aa7e7936565054e8ceda454caf80dcb44a5
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2026-03-09 08:00:17 -0300

    feat: support new character properties, fix related UI issues

--- src/heroes/actions/get-all-teams.action.ts
+++ src/heroes/actions/get-all-teams.action.ts
@@ -1,25 +1,25 @@
-import { getAllHeroesAction } from "./get-all-heroes.action";
+// import { getAllHeroesAction } from "./get-all-heroes.action";
 
-export type TeamsObject = Record<string, string[]>; // key = equipo, value = array de héroes
+// export type TeamsObject = Record<string, string[]>; // key = equipo, value = array de héroes
 
-export const getTeamsObject = async (): Promise<TeamsObject> => {
-	const heroes = await getAllHeroesAction();
+// export const getTeamsObject = async (): Promise<TeamsObject> => {
+// 	const heroes = await getAllHeroesAction();
 
-	const teams: TeamsObject = {};
+// 	const teams: TeamsObject = {};
 
-	heroes.forEach((hero) => {
-		// Nos aseguramos de que connections y groupAffiliation existan
-		const affiliations = hero.connections?.groupAffiliation || [];
+// 	heroes.forEach((hero) => {
+// 		// Nos aseguramos de que connections y groupAffiliation existan
+// 		const affiliations = hero.connections?.groupAffiliation || [];
 
-		affiliations.forEach((team) => {
-			// Si el equipo no existe en el objeto, lo creamos con un array vacío
-			if (!teams[team]) {
-				teams[team] = [];
-			}
-			// Agregamos el alias o nombre del héroe al equipo
-			teams[team].push(hero.alias || hero.name);
-		});
-	});
+// 		affiliations.forEach((team) => {
+// 			// Si el equipo no existe en el objeto, lo creamos con un array vacío
+// 			if (!teams[team]) {
+// 				teams[team] = [];
+// 			}
+// 			// Agregamos el alias o nombre del héroe al equipo
+// 			teams[team].push(hero.alias || hero.name);
+// 		});
+// 	});
 
-	return teams;
-};
+// 	return teams;
+// };
