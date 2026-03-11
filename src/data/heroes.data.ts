0da070b7987f2bf1e11d315635e764b4553d1930
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2026-03-11 14:12:19 -0300

    feat: support new character properties, fix related UI issues

--- src/data/heroes.data.ts
+++ src/data/heroes.data.ts
@@ -1,14 +1,14 @@
-import { Hero } from 'src/heroes/entities/hero.entity';
-import { charactersOne } from './characters_one.data';
-import { charactersTwo } from './characters_two.data';
-import { moreVillains } from './more_villains.data';
-import { moreAntiheroes } from './more_antiheroes.data';
-import { moreHeroes } from './more_heroes.data';
+import { charactersOne } from "./characters_one.data";
+import { charactersTwo } from "./characters_two.data";
+import { moreVillains } from "./more_villains.data";
+import { moreAntiheroes } from "./more_antiheroes.data";
+import { moreHeroes } from "./more_heroes.data";
+import type { Hero } from "@/heroes/types/hero.interface";
 
 export const heroesData: Hero[] = [
-  ...charactersOne,
-  ...charactersTwo,
-  ...moreVillains,
-  ...moreAntiheroes,
-  ...moreHeroes,
+	...charactersOne,
+	...charactersTwo,
+	...moreVillains,
+	...moreAntiheroes,
+	...moreHeroes,
 ];
